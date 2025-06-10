c25dc49f9ed6b212be2a2144a82fd578d35cddf0
Josh Yeaton <yeaton.josh@gmail.com>
2025-06-10 18:38:48 -0400

    Add solutions for first js probs

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -8,6 +8,15 @@
 */
 
 // Add your code right below, good luck!
+let firstName = "Carlos"
+let lastName = "Stevenson"
+let birthYear = 1947
+let thisYear = 1965
+let fullName = firstName + " " + lastName
+let age = thisYear - birthYear
+let greeting = "Hello! My name is " + fullName + " and I am " + age + " years old."
+
+console.log(greeting)
 
 
 
